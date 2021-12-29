9323dd9efc3f08f3a4773c8f33cab30e595c485f
wilson <zxc0905735990@gmail.com>
2021-12-29 10:25:26 +0800

    init

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import React from 'react';
 
 export default function App() {
+  const [food, setFood ] = useState('candy')
+  const [count, setCount ] = useState(0)
+  const changeFood=()=>{
+    setFood('noodles')
+    console.log('按到noodles按鈕了')
+  }
+  const Counter=()=>{
+    setCount(count+1)
+    console.log('按到計數器了')
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.mainText}>Open up App.js to start working on your react!</Text>
+      <Text>{food}</Text>
+      <Text>計數器有{count}個</Text>
+      <Button 
+        title='change food' 
+        onPress={() => changeFood()}
+      />
+      <Button 
+        title='change count' 
+        onPress={() => Counter()}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +39,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  mainText:{
+    fontSize:20,
+    textAlign: 'center',
+    color:'red',
+  },
+  SecText:{
+    color:'green'
+  }
 });
